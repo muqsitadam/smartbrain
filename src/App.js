a42eb481be442d08eb94dae8db4f55812e230495
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-06-05 23:21:22 +0100

    Added clarifai to my project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ import Particle from './components/Particles/Particles';
 import './App.css';
 
 class App extends Component{
+constructor(){
+  super();
+  this.state = {
+    input: '',
+  }
+}
+
+onInputChange = (event) =>{
+  console.log(event.target.value)
+}
+
+onButtonSubmit= () => {
+  console.log('click')
+}
+
   render(){
     return(
       <div className="App">
@@ -14,7 +29,9 @@ class App extends Component{
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
         {/*<FaceRecognition />*/}
       </div>
     );
