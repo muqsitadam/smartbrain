944c06c87359d623221c733a454bc7810d9f7725
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-06-11 14:55:10 +0100

    Made the face recognition box

--- src/App.js
+++ src/App.js
@@ -18,8 +18,27 @@ constructor(){
   super();
   this.state = {
     input: "",
-    imageUrl: ""
+    imageUrl: "",
+    box: {}
   }
+}
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+  const image = document.getElementById('inputimage')
+  const width = Number(image.width)
+  const height = Number(image.height)
+  return{
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.right_col * height),
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box)
+  this.setState({box: box})
 }
 
 onInputChange = (event) =>{
@@ -33,16 +52,10 @@ onButtonSubmit= () => {
   })
   console.log('click')
   apps.models.predict(
-    Clarifai.FACE_DETECT_MODEL,
-    this.state.input)
-    .then(
-    function(response){
-      console.log(response.outputs)
-    },
-    function(err){
-      console.log(err)
-    }
-  )
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
 }
 
   render(){
@@ -55,7 +68,7 @@ onButtonSubmit= () => {
         <ImageLinkForm 
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
