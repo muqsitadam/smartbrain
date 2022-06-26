5b690e2d34180c6f852d63efb83b374326485050
Abdulmuqsit Adam <muqsitadam@gmail.com>
2022-06-26 22:47:22 +0100

    Made appropriate changes to go conform with server

--- src/App.js
+++ src/App.js
@@ -23,8 +23,27 @@ constructor(){
     imageUrl: "",
     box: {},
     route: 'signIn',
-    isSignedIn: false
+    isSignedIn: false,
+    user: {
+      id: "",
+      name: "",
+      email: "",
+      entries: 0,
+      joined: ''
+    } 
   }
+}
+
+loadUser = (data) => {
+  this.setState({
+    user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    } 
+  })
 }
 
 calculateFaceLocation = (data) => {
@@ -81,15 +100,24 @@ onRouteChange = (route) => {
         {route === 'home'
         ? <div>
             <Logo />
-            <Rank />
+            <Rank
+            name={this.state.user.name}
+            entries={this.state.user.entries}
+            />
             <ImageLinkForm 
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit} />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div> 
         : (route === 'signIn'
-          ?<SignIn onRouteChange = {this.onRouteChange} />
-          :<Register onRouteChange = {this.onRouteChange} /> 
+          ? <SignIn 
+            loadUser={this.loadUser}
+            onRouteChange = {this.onRouteChange} 
+           />
+          :<Register 
+            loadUser={this.loadUser}
+            onRouteChange = {this.onRouteChange} 
+          /> 
           )
         }
       </div>
